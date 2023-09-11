2e454ff72324dc1c64fbbcd9fa08353594e04d29
20193336 leesungjin <sjin0902@naver.com>
2023-09-11 16:41:28 +0900

    first commit

---
+++ src/JoinSum.java
@@ -0,0 +1,3 @@
+public class JoinSum extends Thread{
+
+}
---
+++ src/TestThread.java
@@ -0,0 +1,19 @@
+class MyRunnable implements Runnable {
+    String myName;
+    public MyRunnable(String name) {
+        myName = name;
+    }
+    public void run() {
+        for(int i=0;i<=10;i++)
+            System.out.print(myName+i+" ");
+    }
+}
+
+public class TestThread {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyRunnable(  "A"));
+        Thread t2 = new Thread(new MyRunnable("B"));
+        t1.start();
+        t2.start();
+    }
+}
